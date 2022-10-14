83f1cae0a7649b099bda58a7c9441dd5aa167a6c
Esen888 <esensharsheev808@gmail.com>
2022-10-14 18:10:21 +0600

    practice3

---
+++ practice.dart
@@ -0,0 +1,80 @@
+void main() {
+// int a = 2;
+// int b = 2;
+// int c = a +b;
+// print("$a + $b = $c");
+
+
+// int a = 5;
+// int b = 10;
+// int c = a - b;
+// print("$a - $b = $c");
+
+
+
+
+// int a = 16;
+// int h = 3;
+// double j = g * h;
+// print('$g * $h = $j');
+
+
+
+// int a = 18;
+// int b = 2;
+// double c = a / b;
+// print('$a / $b = $c');
+
+
+
+
+
+// int a = 12;
+// int b = 15;
+// bool c = a>b;
+// print("a > b = $c");
+
+// int a = 12;
+// int b = 15;
+// bool c = a<b;
+// print("a < b = $c");
+
+// int a = 12;
+// int b = 15;
+// bool c = a<= b;
+// print("a <= b = $c");
+
+
+// int a = 12;
+// int b = 15;
+// bool c = a>= b;
+// print("a>= b = $c");
+
+
+// int a = 12;
+// int b = 15;
+// bool c = a == b;
+// print("a == b = $c");
+
+
+// int a = 10;
+// print(a.toDouble());
+
+// double a = 10.1;
+// print(a.toInt());
+
+int a = 5;
+print(a.toString());
+
+
+// String name = 'Esen';
+// print(name.length);
+
+
+// List array = ["Esen", " ", "Sharsheev"];
+// print(array.first);
+
+
+
+
+}
